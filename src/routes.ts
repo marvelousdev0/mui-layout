eff02fe65516bf718a3d40798736e8cc2538743a
Marvelous Dev <marvelousdev0@gmail.com>
2022-05-31 09:30:44 -0400

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -3,5 +3,5 @@ import Jobs from './pages/Jobs'
 
 export const routes = [
   { label: 'Home', path: '/', element: Home },
-  { label: 'Jobs', path: '/jobs', element: Jobs }
+  { label: 'Jobs', path: 'jobs', element: Jobs }
 ]
